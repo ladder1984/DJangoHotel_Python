a4d79fc00c696b65e9a75a2802c93afa023aed84
ladder1984 <ladder1984@gmail.com>
2014-09-13 15:55:07 +0800

    commit

---
+++ kcsj/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import include, url, patterns
+
+from django.contrib import admin
+
+from DJangoHotel.viewspackage.aboutView import about
+from DJangoHotel.viewspackage.indexView import index
+from DJangoHotel.viewspackage.orderResultView import orderResult
+from DJangoHotel.viewspackage.orderView import order
+from DJangoHotel.viewspackage.roomInfoView import roomInfo
+from django.conf import settings
+from django.conf.urls.static import static
+from DJangoHotel.viewspackage.testView import test
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'kcsj.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^about/', about),
+    url(r'^roominfo/', roomInfo),
+    url(r'^test/', test),
+    url(r'^$', index),
+    url(r'^order/',order),
+    url(r'^orderresult/',orderResult)
+]
+
+
